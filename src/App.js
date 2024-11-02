e6693e6a76d6cc33d8f9dc9bc2b6d38423e473b4
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-11-02 16:35:56 +0100

    design code in preview with hightlightJs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import MarkdownPreviewer from './components/MarkdownPreviewer';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/default.css'; // Importez le style par défaut ou un autre style de votre choix
 
 function App() {
+  useEffect(() => {
+    document.querySelectorAll("pre code").forEach((block) => {
+      hljs.highlightBlock(block);
+    });
+  }, []);
+
   return (
     <div className="App">
       <MarkdownPreviewer />
